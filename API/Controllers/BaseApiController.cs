0bdbf5c9ad02965683d88533b9538766b04618bb
MausVII <valscatty@gmail.com>
2022-06-22 20:10:01 -0500

    First Commit

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    // Controller is a placeholder that get replaced by the class name minus 'Controller'
+    [Route("api/[controller]")]
+    public class BaseApiController : ControllerBase
+    {
+        
+    }
+}
